63b94057c52846f44a2775d241fa26bf74c42f84
unknown <peiyuanc@sfu.ca>
2023-03-10 23:25:45 -0800

    2

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,20 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "function": {
+    "S3Trigger93c80eb9": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "imageOnly": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
